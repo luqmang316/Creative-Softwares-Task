edecbbe2c73293248b6ac2a44f99d06c3443498f
luqmang316 <luqmang316@gmail.com>
2020-10-29 10:34:39 -0500

    Initial Commit

---
+++ Creative Softwares Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Creative Softwares Task
+//
+//  Created by Muhammad Luqman on 10/29/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
